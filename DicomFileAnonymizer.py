d6443dedbd2c9bf92e9b39324491076b56c765cf
Sebastian Cannas <jscanass@gmail.com>
2018-04-19 21:57:35 -0500

    Update DicomFileAnonymizer.py

--- DicomFileAnonymizer.py
+++ DicomFileAnonymizer.py
@@ -1,26 +1,31 @@
 import pydicom
 from pydicom.data import get_testdata_files
 
-path = #file_path
+def DicomFileAnonymizer(path):
+    """Args:
+           path(str): The path of the DICOM file 
+        Returns:
+            DICOM file saved without personal information"""
+        
 
-filename = get_testdata_files(path)
-dataset = pydicom.dcmread(path)
-
-data_elements = ['PatientName','PatientID','IssuerOfPatientID','TypeOfPatientID',
-                 'PatientBirthDate','PatientBirthTime','PatientBirthDateInAlternative',
-                 'PatientDeathDateInAlternative','PatientDeathDateInAlternative',
-                 'PatientSex','PatientInsurancePlanCode','OtherPatientIDs','PatientAge',
-                 'PatientSize','PatientWeight','PatientAddress','InsurancePlanIdentification',
-                 'MilitaryRank','BranchOfService','PatientTelephoneNumbers',
-                 'AdditionalPatientHistory']
-
-"""
-You can add or remove DICOM data to delete  based on 'Keywords' of 
-http://dicom.nema.org/medical/dicom/current/output/pdf/part06.pdf
-(personal information (0010,xxxx) pag. 29-31) 
-"""
-
-for element in data_elements:
-    if element in dataset:
-        delattr(dataset, element)
-dataset.save_as(path)
+    filename = get_testdata_files(path)
+    dataset = pydicom.dcmread(path)
+    
+    data_elements = ['PatientName','PatientID','IssuerOfPatientID','TypeOfPatientID',
+                    'PatientBirthDate','PatientBirthTime','PatientBirthDateInAlternative',
+                    'PatientDeathDateInAlternative','PatientDeathDateInAlternative',
+                    'PatientSex','PatientInsurancePlanCode','OtherPatientIDs','PatientAge',
+                    'PatientSize','PatientWeight','PatientAddress','InsurancePlanIdentification',
+                    'MilitaryRank','BranchOfService','PatientTelephoneNumbers',
+                    'AdditionalPatientHistory']
+                    
+    """
+    You can add or remove elements to delete  based on 'keywords' of 
+    http://dicom.nema.org/medical/dicom/current/output/pdf/part06.pdf
+    (personal information (0010,xxxx) pag. 29-31) 
+    """
+    
+    for element in data_elements:
+        if element in dataset:
+            delattr(dataset, element)
+    return dataset.save_as(path)
