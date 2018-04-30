c28a7e45dac61dc952dbc15bcbf7bafcdcd0a12f
Sebastián Cañas <jscanass@gmail.com>
2018-04-30 10:01:15 -0500

    Update DicomReader.py

--- DicomReader.py
+++ DicomReader.py
@@ -1,7 +1,6 @@
 import pydicom
 from pydicom.errors import InvalidDicomError
 
-file_ = "/Users/jscs/DICOM/Untitled.rtf"
 def DicomReader(file_):
     try:
         pydicom.dcmread(file_)
@@ -9,4 +8,3 @@ def DicomReader(file_):
         print('Not Dicom file')
     else:
         print('Dicom file')
-
