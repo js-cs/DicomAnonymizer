9a5e06724b30a9f05751bb7aa02965d68028f331
Sebastián Cañas <jscanass@gmail.com>
2018-04-29 19:24:47 -0500

    Update DicomReader.py

--- DicomReader.py
+++ DicomReader.py
@@ -1,23 +1,12 @@
 import pydicom
 from pydicom.data import get_testdata_files
-from pydicom import dcmread, filereader
+from pydicom.errors import InvalidDicomError
 
 folder = '/Users/jscs/DICOM/Untitled.rtf' #Cualquier archivo no dicom
 
 try:
     pydicom.dcmread(folder)
 except InvalidDicomError: 
-    print('sfs')    #se espera que se ejecute cuando no es archivo dicom
+    print('sfs')
 else:
-    print('woj') #lo que se ejecuta si es archivo dicom
-
-    
-#un ejemplo que sí funciona con una error por defecto
-""" 
-try:
-    1/0
-except ZeroDivisionError:
-    print('juak')
-else:
-    print('woj')
-    """
+    print('woj') 
