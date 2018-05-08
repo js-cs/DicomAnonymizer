a29bac23a09bbb7d5898573085ab3464e79ea246
Sebastián Cañas <jscanass@gmail.com>
2018-05-07 23:38:27 -0500

    Update WalkingDirectory.py

--- WalkingDirectory.py
+++ WalkingDirectory.py
@@ -1,13 +1,16 @@
 import os
-import pydicom
-from DicomReader import *
-from pydicom.data import get_testdata_files
-from pydicom.errors import InvalidDicomError
 
-path = '/Users/jscs/Downloads/test'
-for(root,dirs,files) in os.walk(path):
-    print(root,'+++++++++++++++++++++++++++++++**+++++++++++++++++++++++++++++')
-    for(file) in files:
-        print(file)
-        DicomReader(root+'/'+file)
-        
+
+def walking(folder, applied_function):
+    """
+    Walks through folder doing an specified function in the second parameter
+    :type applied_function: function
+    :param folder: the folder to execute the walking
+    :param applied_function: specified function to do on the walking
+    Example walking('/Users/jscs/Downloads/test', print)
+    """
+    for (root, dirs, files) in os.walk(folder):
+        print('+++++++++++++**++++++++++++', 'Walking through', root, '+++++++++++++**+++++++++++++')
+        for (file) in files:
+            applied_function(os.path.join(root, file))
+
