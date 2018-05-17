6762a2a4efc2900053448ce1dff2bea9134d3ee3
Sebastián Cañas <jscanass@gmail.com>
2018-05-16 19:14:29 -0500

    Update MainDicomAn.py

--- MainDicomAn.py
+++ MainDicomAn.py
@@ -1,46 +1,10 @@
 import os
 import os.path
-import zipfile
-import pydicom
 import patoolib
+import nibabel as nib
+import pydicom
 from pydicom.errors import InvalidDicomError
-from pydicom.data import get_testdata_files
-
-# problema: formatos
-
-def unzip(path, path_out):
-    if zipfile.is_zipfile(path):
-        zip_ref = zipfile.ZipFile(path, 'r')
-        zip_ref.extractall(path_out)
-        zip_ref.close()
-
-
-def read_unpack(file_path):
-    """
-    :param file_path: file to unpack
-    :param output_folder: folder to save file unpacked (Whish: =os.path.abspath(os.path.join(file_path, os.pardir)))
-    :return: Message of unpacked or no file detected
-
-    """
-    ends = ['.rar', '.7z', '.dmg', '.gz', '.iso', '.tar', '.zip','.bz2']
-    output_folder = os.path.abspath(os.path.join(file_path, os.pardir))
-    if any(file_path.lower().endswith(end) for end in ends):
-        #if read pydicom or nifti
-        if patoolib.extract_archive(file_path, outdir=output_folder):
-            return 'Done'
-    # else:
-        #  raise ValueError('File extension is not supported')
-    else:
-        pass
-
-
-def dicom_reader(file_):
-    try:
-        pydicom.dcmread(file_)
-    except InvalidDicomError:
-        return False
-    else:
-        return True
+from nibabel.loadsave import ImageFileError
 
 
 def walking(folder, applied_function):
@@ -72,42 +36,105 @@ def recursive_walking(folder):
     return
 
 
+def read_unpack(file_path):
+    """
+    Detect and unpack compressed files
+    :param file_path: file to unpack
+    :return: Message of unpacked in the parent directory or no file detected
+    """
+    ends = ['.rar', '.7z', '.dmg', '.gz', '.iso', '.tar', '.zip', '.bz2', '.xz', '.wim', '.swm', '.esd', '.cb7', '.cbr',
+            '.jar', '.cbz', '.rz', '.z', '.arc', '.ace', '.dmg', '.lz', '.lzma', '.lz', '.lrz', '.iso', '.dms', '.a']
+    output_folder = os.path.abspath(os.path.join(file_path, os.pardir))
+    if any(file_path.lower().endswith(end) for end in ends):
+        if patoolib.extract_archive(file_path, outdir=output_folder):
+            return print(file_path, "successfully unpacked")
+        else:
+            raise ValueError('File extension is not supported for your program', file_path)
+    else:
+        pass
+
+
+def read_delete(file_path):
+    """
+    Detect and remove packed files
+    :param file_path: file to remove
+    :return: Message of deleted packed file, nifti-dicom recognition, or no file detected
+
+    """
+    ends = ['.rar', '.7z', '.dmg', '.gz', '.iso', '.tar', '.zip', '.bz2', '.xz', '.wim', '.swm', '.esd', '.cb7', '.cbr',
+            '.jar', '.cbz', '.rz', '.z', '.arc', '.ace', '.dmg', '.lz', '.lzma', '.lz', '.lrz', '.iso', '.dms', '.a']
+    if any(file_path.lower().endswith(end) for end in ends):
+        try:
+            nib.load(file_path)
+        except ImageFileError:
+            try:
+                pydicom.dcmread(file_path)
+            except InvalidDicomError:
+                os.remove(file_path)
+                return print('Packed file removed: ', file_path)
+            else:
+                return print('Dicom file does not removed: ', file_path)
+        else:
+            return print('Nifti file does not removed: ', file_path)
+    else:
+        pass
+
+
+def dicom_reader(file_path):
+    try:
+        pydicom.dcmread(file_path)
+    except InvalidDicomError:
+        return False
+    else:
+        return True
+
+
 def dicom_file_anonymizer(path):
     """Args:
            path(str): The path of the DICOM file 
         Returns:
             DICOM file saved without personal information"""
 
-    filename = get_testdata_files(path)
-    data_set = pydicom.dcmread(path)
+    if dicom_reader(path):
+        data_set = pydicom.dcmread(path)
+        print('---> Reading to anonymize: ', path)
 
-    data_elements = ['PatientName', 'PatientID', 'IssuerOfPatientID', 'TypeOfPatientID',
-                     'PatientBirthDate', 'PatientBirthTime', 'PatientBirthDateInAlternative',
-                     'PatientDeathDateInAlternative', 'PatientDeathDateInAlternative',
-                     'PatientSex', 'PatientInsurancePlanCode', 'OtherPatientIDs', 'PatientAge',
-                     'PatientSize', 'PatientWeight', 'PatientAddress', 'InsurancePlanIdentification',
-                     'MilitaryRank', 'BranchOfService', 'PatientTelephoneNumbers',
-                     'AdditionalPatientHistory']
+        data_elements = ['PatientName', 'PatientID', 'IssuerOfPatientID', 'TypeOfPatientID',
+                         'PatientBirthDate', 'PatientBirthTime', 'PatientBirthDateInAlternative',
+                         'PatientDeathDateInAlternative', 'PatientDeathDateInAlternative',
+                         'PatientSex', 'PatientInsurancePlanCode', 'OtherPatientIDs', 'PatientAge',
+                         'PatientSize', 'PatientWeight', 'PatientAddress', 'InsurancePlanIdentification',
+                         'MilitaryRank', 'BranchOfService', 'PatientTelephoneNumbers',
+                         'AdditionalPatientHistory']
 
-    """
-    You can add or remove elements to delete  based on 'keywords' of 
-    http://dicom.nema.org/medical/dicom/current/output/pdf/part06.pdf
-    (personal information (0010,xxxx) pag. 29-31) 
-    """
+        """
+        You can add or remove elements to delete  based on 'keywords' of 
+        http://dicom.nema.org/medical/dicom/current/output/pdf/part06.pdf
+        (personal information (0010,xxxx) pag. 29-31) 
+        """
 
-    for element in data_elements:
-        if element in data_set:
-            delattr(data_set, element)
-    return data_set.save_as(path)
+        for element in data_elements:
+            if element in data_set:
+                delattr(data_set, element)
+                print('Deleting', element, 'from', path)
+            else:
+                print(element, 'not in', path)
+        print('---> Finished anonymization in ', path)
+        return data_set.save_as(path)
+    else:
+        pass
 
 
-def dicom_anonymizer(path):
-    print('--> Searching zip files and unzipping')
-    walking(path, unzip)
-    print('--> Searching Dicom files and Anonymizing')
-    walking(path, dicom_file_anonymizer)
+def dicom_anonymizer(directory_path):
+    print('--> Searching compressed files and unpacking')
+    walking(directory_path, read_unpack)
+    print('--> Searching compressed files unpacked and deleting')
+    walking(directory_path, read_delete)
+    print('--> Searching dicom files and anonymizing')
+    walking(directory_path, dicom_file_anonymizer)
+    print('--> Successful anonymization')
 
 
-path = '/Users/jscs/Downloads/test'
+directory_path_test = '/Users/jscs/Downloads/test'
 
-walking(path, read_unpack)
+dicom_anonymizer(directory_path_test)
