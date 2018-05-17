01df4cacf8bdc880322e3d461455c6c36276e7ed
Sebastián Cañas <jscanass@gmail.com>
2018-05-16 19:16:06 -0500

    Update and rename unzip.py to unpack.py

--- unpack.py
+++ unpack.py
@@ -1,6 +1,8 @@
 import os
 import fleep
+import zipfile
 import patoolib
+from patoolib.util import PatoolError
 
 
 def unpack(input_file, output_folder):
@@ -9,8 +11,8 @@ def unpack(input_file, output_folder):
     :param output_folder: folder to save file unpacked
     :return: Message of unpacked or no file detected
 
-    Beside the packages above, you must have installed patool
     """
+    print(input_file)
     if os.path.isdir(input_file):
         return False
 
@@ -26,9 +28,26 @@ def unpack(input_file, output_folder):
     elif set(info.extension) & extensions_supported:
         print(set(info.extension) & extensions_supported)
         print('---> File recognized, unpacking <---')
-        patoolib.extract_archive(input_file, outdir=output_folder)
+        try:
+            patoolib.extract_archive(input_file, outdir=output_folder)
+        # probably a false positive
+        except PatoolError:
+            return False
         result = True
     else:
         result = False
     return result
-            
+
+
+def unzip(path, path_out):
+    if zipfile.is_zipfile(path):
+        zip_ref = zipfile.ZipFile(path, 'r')
+        zip_ref.extractall(path_out)
+        zip_ref.close()
+
+"""
+input_file = '/Users/jscs/Downloads/test/faa.rar'
+output_folder = '/Users/jscs/Downloads/test'
+
+unpack(input_file, output_folder)
+"""
